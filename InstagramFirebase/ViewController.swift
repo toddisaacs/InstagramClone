7d8d06e3496969973c7bba33b8eaa1116ab77f31
Todd Isaacs <teisaacs21w@gmail.com>
2018-09-30 12:43:27 -0400

    Added login page

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -10,9 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  let plusPhotoButton: UIButton = {
+    let button = UIButton(type: .system)
+    let image = #imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal)
+    button.setImage(image, for: .normal)
+    
+    return button
+  }()
+  
+  let emailTextField: UITextField = {
+    let tf = UITextField()
+    tf.placeholder = "Email"
+    tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+    tf.borderStyle = .roundedRect
+    tf.font = .systemFont(ofSize: 14)
+    
+    return tf
+  }()
+  
+  let usernameTextField: UITextField = {
+    let tf = UITextField()
+    tf.placeholder = "Username"
+    tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+    tf.borderStyle = .roundedRect
+    tf.font = .systemFont(ofSize: 14)
+    
+    return tf
+  }()
+  
+  let passwordTextField: UITextField = {
+    let tf = UITextField()
+    tf.placeholder = "Password"
+    tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+    tf.borderStyle = .roundedRect
+    tf.font = .systemFont(ofSize: 14)
+    tf.isSecureTextEntry = true
+    
+    return tf
+  }()
+  
+  let signUpButton:UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("Sign Up", for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+    button.setTitleColor(.white, for: .normal)
+    button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
+    button.layer.cornerRadius = 5
+    return button
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    view.addSubview(plusPhotoButton)
+    plusPhotoButton.anchor(top: view.topAnchor,
+                           left: nil,
+                           bottom: nil,
+                           right: nil,
+                           paddingTop: 40,
+                           paddingLeft: 0,
+                           paddingBottom: 0,
+                           paddingRight: 0,
+                           width: 140,
+                           height: 140)
+    plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+    
+    setupInputFields()
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +84,21 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  fileprivate func setupInputFields() {
+    let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
+    
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.distribution = .fillEqually
+    stackView.axis = .vertical
+    stackView.spacing = 10
+    
+    view.addSubview(stackView)
+    
+    stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20).isActive = true
+    stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+    stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+    stackView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+  }
 
 }
 
---
+++ InstagramFirebase/utils/Extensions.swift
@@ -0,0 +1,56 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by Isaacs, Todd (CAI - Carmel) on 9/30/18.
+//  Copyright © 2018 Isaacs, Todd. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+    return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+  }
+}
+
+extension UIView {
+  func anchor(top: NSLayoutYAxisAnchor?,
+              left: NSLayoutXAxisAnchor?,
+              bottom: NSLayoutYAxisAnchor?,
+              right: NSLayoutXAxisAnchor?,
+              paddingTop: CGFloat,
+              paddingLeft: CGFloat,
+              paddingBottom: CGFloat,
+              paddingRight:CGFloat,
+              width: CGFloat,
+              height: CGFloat) {
+    
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    if let top = top {
+      topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+    }
+    
+    if let left = left {
+      leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+    }
+    
+    if let bottom = bottom {
+      bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+    }
+    
+    if let right = right {
+      rightAnchor.constraint(equalTo: right, constant: paddingRight).isActive = true
+    }
+    
+    if width != 0 {
+      widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    
+    if height != 0 {
+      heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+  }
+}
