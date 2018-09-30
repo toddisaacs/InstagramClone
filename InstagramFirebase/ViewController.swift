25786d2aeb31d7fde19a0b6cf862a747e602a4d6
Todd Isaacs <teisaacs21w@gmail.com>
2018-09-30 09:07:52 -0400

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Isaacs, Todd (CAI - Carmel) on 9/30/18.
+//  Copyright © 2018 Isaacs, Todd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
